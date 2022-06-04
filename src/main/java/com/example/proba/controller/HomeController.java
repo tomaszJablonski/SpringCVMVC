30c3b0cb9253ef8ee176873e038a8eab8453fb2b
MrMag1990 <MrMag1990@kursSDA.pl>
2022-06-04 11:18:12 +0200

    simple change

--- src/main/java/com/example/proba/controller/HomeController.java
+++ src/main/java/com/example/proba/controller/HomeController.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @Secured({"ROLE_ADMIN"})
+
     @GetMapping("/")
     public String getIndex(){
         return "/index";
     }
 
-    @Secured({"ROLE_ADMIN"})
     @GetMapping("/index")
     public String getAbout(){
         return "/index";
